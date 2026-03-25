e5309486f26e450b0d5ab581fa9f59cf0c7eb3a2
ss0834-star <ss0834@srmist.edu.in>
2026-03-25 08:49:44 +0530

    Add Passenger Bogies to Train

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -3,9 +3,25 @@ import java.util.List;
 
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
+
         System.out.println("=== Train Consist Management App ===");
+
         List<String> bogies = new ArrayList<>();
         System.out.println("Train consist initialized.");
         System.out.println("Initial bogie count: " + bogies.size());
+
+        List<String> passengerBogies = new ArrayList<>();
+
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+
+        System.out.println("Passenger Bogies: " + passengerBogies);
+
+        passengerBogies.remove("AC Chair");
+
+        System.out.println("After Removal: " + passengerBogies);
+
+        System.out.println("Sleeper exists: " + passengerBogies.contains("Sleeper"));
     }
 }
